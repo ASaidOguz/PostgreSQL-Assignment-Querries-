2876eab381574950081467eea512c6dc4a198210
ASaidOguz <ahmetsaid2136@gmail.com>
2022-04-21 02:58:30 +0300

    initial commit

--- patika-sql-ödev5.sql
+++ patika-sql-ödev5.sql
@@ -1,4 +1,17 @@
+--Soru 1
 SELECT * FROM film
 WHERE title LIKE '%n'
 ORDER BY length DESC
 LIMIT 5;
+
+--Soru 2;
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC
+LIMIT 5
+OFFSET 5;
+
+--Soru 3;
+SELECT last_name FROM customer 
+WHERE store_id=1
+ORDER BY last_name ASC;
